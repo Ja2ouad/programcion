3ae85eb6f837ca29399f6753a93d5ccb56d63ca4
Mañana <Maana@AEMCENT-VL4EIA2>
2024-05-02 10:57:40 +0200

    commit inicial primera version de la clase

---
+++ PruebeCommits/src/puebacommits/PruebaCommits.java
@@ -0,0 +1,10 @@
+package puebacommits;
+
+public class PruebaCommits {
+
+	public static void main(String[] args) {
+		System.out.println("Hola esta es es la primera version de la clase");
+
+	}
+
+}
