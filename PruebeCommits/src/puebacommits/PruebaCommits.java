95b2cd0e40f5b3c8043c23de530af1982e901146
Mañana <Maana@AEMCENT-VL4EIA2>
2024-05-02 12:03:28 +0200

    la tercera version de la clase

--- PruebeCommits/src/puebacommits/PruebaCommits.java
+++ PruebeCommits/src/puebacommits/PruebaCommits.java
@@ -3,7 +3,7 @@ package puebacommits;
 public class PruebaCommits {
 
 	public static void main(String[] args) {
-		System.out.println("Hola esta  es la primera version de la clase");
+		System.out.println("Hola esta  es la tercera version de la clase");
 
 	}
 
